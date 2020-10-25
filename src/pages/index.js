bc6ac1616e5801235453f7884f2bf3447d20de9a
Ryan Tin <ryantin1995@gmail.com>
2020-10-26 00:55:35 +0800

    updated home details

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,6 @@ const IndexPage = () => {
             <Head title="Home" />
             <h1>Hello.</h1>
             <h2>I'm Ryan, a full-stack developer.</h2>
-            <p>Need a developer? <Link to="/contact">Contact me.</Link></p>
         </Layout>
     );
 };
